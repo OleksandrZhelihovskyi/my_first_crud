44331352826a82a0aeb15f867ed03eb243141b34
OleksandrZhelihovskyi <zhelihovskii.aleksandr@gmail.com>
2023-10-04 12:58:12 +0300

    App.js fixed

--- App.js
+++ App.js
@@ -1,13 +1,12 @@
 const Koa = require('koa');
-const {koaBody} = require("koa-body")
+const { koaBody } = require("koa-body")
 const router = require('./router/index')
-const app = new Koa();
 const cors = require('@koa/cors');
 
+const app = new Koa();
 
 app.use(cors());
 app.use(koaBody());
 app.use(router.routes())
 app.use(router.allowedMethods())
-//app.use(json())
-app.listen(2000, ()=>{console.log('http:localhost:2000')});
+app.listen(2000, () => { console.log('http:localhost:2000') });
