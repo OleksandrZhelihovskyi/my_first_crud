fddaeb68747be6e58e3fd19803a61282f6035051
OleksandrZhelihovskyi <zhelihovskii.aleksandr@gmail.com>
2023-10-06 13:31:07 +0300

    final fix

--- api/crud.js
+++ api/crud.js
@@ -19,73 +19,48 @@ exports.getTask = async (data) => {
     }
     catch (err) {
         client.release()
-        return err
+        return err.detail
 
     }
 };
 exports.putTask = async (data) => {
     try {
         const client = await pool.connect();
-        const text = 'SELECT user_data.email FROM user_data WHERE user_data.email = $1'
-        const values = data.email
-        const res = await client.query(text, [values])
-        if (data.username && data.email && data.password && !res.rows[0]) {
-            const query = 'INSERT INTO user_data (username,email,password) VALUES($1::text,$2::text,$3::text)'
-            const values = [data.username, data.email, data.password]
 
-            const result = await client.query(query, values);
-        } else {
-            client.release()
-            throw ('Wrong data')
-        }
+        const query = 'INSERT INTO user_data (username,email,password) VALUES($1::text,$2::text,$3::text)'
+        const values = [data.username, data.email, data.password]
+        const result = await client.query(query, values);
         client.release()
         return 'Success'
     }
     catch (err) {
-        return err
+        return err.detail
     }
 };
 
 exports.updateTask = async (data) => {
     try {
-        const values = data.email
         const client = await pool.connect();
-        const to_update = await client.query('select user_data.email from user_data where user_data.email = $1',
-            [values]);
 
-        if (to_update.rows[0]) {
-            console.log(to_update.rows[0])
-            const result = await client.query('UPDATE user_data SET username = $1::text, email = $2::text, password = $3::text WHERE email = $4::text',
-                [data.username, data.email, data.password, to_update.rows[0].email]
-            );
-            console.log(result)
-        } else {
-            client.release()
-            throw ('Wrong data')
-        }
+        const result = await client.query('UPDATE user_data SET username = $1::text, email = $2::text, password = $3::text WHERE email = $4::text',
+            [data.username, data.email, data.password, to_update.rows[0].email]
+        );
         client.release()
         return ('Success')
     }
     catch (err) {
-        return err
+        return err.detail
     }
 };
 exports.deleteTask = async (id) => {
     try {
         const client = await pool.connect();
-        const to_delete = await client.query('select user_data.id from user_data where user_data.id = $1', [id]);
-        if (to_delete.rows[0]) {
-            const result = await client.query('DELETE FROM user_data WHERE user_data.id = $1', [id]
-            );
-        } else {
-            client.release()
-            throw ('Wrong data')
-        }
+        const result = await client.query('DELETE FROM user_data WHERE user_data.id = $1', [id]
+        );
         client.release()
         return 'Success'
-
     }
     catch (err) {
-        return err
+        return err.detail
     }
 };
--- router/index.js
+++ router/index.js
@@ -4,51 +4,23 @@ const Task = require('../api/crud');
 
 
 
-router.get('/api/tasks', async (ctx, next) => {
-    try {
+router.get('/api/user-data', async (ctx, next) => {
         const result = await Task.getTask(ctx.request.query);
         ctx.body = result;
-    }
-    catch (err) {
-        await next(err);
-    }
 })
 
 router.put('/api/user-data', async (ctx, next) => {
-    try {
         const result = await Task.putTask(ctx.request.body);
         ctx.body = result;
-    }
-    catch (err) {
-        await next(err);
-    }
 });
 
 router.patch('/api/user-data', async (ctx, next) => {
-    try {
         const result = await Task.updateTask(ctx.request.body);
         ctx.body = result;
-    }
-    catch (err) {
-        await next(err);
-    }
 });
 router.del(`/api/user-data/:id`, async (ctx, next) => {
-    try {
         const result = await Task.deleteTask(ctx.params.id);
         ctx.body = result;
-    }
-    catch (err) {
-        await next(err);
-    }
 });
-router.use((ctx, err) => {
-    console.log('err', err)
-    ctx.status = 500// fixed to 500
-    ctx.body = err
-    
-})
-process.on("uncaughtException", function () {
-    process.exit(); 
-});
+
 module.exports = router;
