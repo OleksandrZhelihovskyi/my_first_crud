51dac9cab41e73141e04f8d32d559bca50d6d150
OleksandrZhelihovskyi <zhelihovskii.aleksandr@gmail.com>
2023-10-06 13:58:56 +0300

    final fix3

--- api/crud.js
+++ api/crud.js
@@ -4,8 +4,7 @@ const pool = new Pool(conf.parsed);
 
 exports.getTask = async (data) => {
     try {
-        const client = await pool.connect();
-        const result = await client.query(
+        const result = await pool.query(
             `select task.title as Task,user_data.username as Name,
         priority.title as Priority,category.title as Category,user_data.photo as Photo
         from task 
@@ -13,24 +12,18 @@ exports.getTask = async (data) => {
         join priority on user_data.id = priority.user_id
         join category on user_data.id = category.user_id; `
         );
-        client.release()
         return result
-
     }
     catch (err) {
-        client.release()
         return err.detail
 
     }
 };
 exports.putTask = async (data) => {
     try {
-        const client = await pool.connect();
-
         const query = 'INSERT INTO user_data (username,email,password) VALUES($1::text,$2::text,$3::text)'
         const values = [data.username, data.email, data.password]
-        const result = await client.query(query, values);
-        client.release()
+        const result = await pool.query(query, values);
         return 'Success'
     }
     catch (err) {
@@ -40,12 +33,9 @@ exports.putTask = async (data) => {
 
 exports.updateTask = async (data) => {
     try {
-        const client = await pool.connect();
-
-        const result = await client.query('UPDATE user_data SET username = $1::text, email = $2::text, password = $3::text WHERE email = $4::text',
-            [data.username, data.email, data.password, to_update.rows[0].email]
+        const result = await pool.query('UPDATE user_data SET username = $1::text, email = $2::text, password = $3::text WHERE email = $2::text',
+            [data.username, data.email, data.password]
         );
-        client.release()
         return ('Success')
     }
     catch (err) {
@@ -54,10 +44,8 @@ exports.updateTask = async (data) => {
 };
 exports.deleteTask = async (id) => {
     try {
-        const client = await pool.connect();
-        const result = await client.query('DELETE FROM user_data WHERE user_data.id = $1', [id]
+        const result = await pool.query('DELETE FROM user_data WHERE user_data.id = $1', [id]
         );
-        client.release()
         return 'Success'
     }
     catch (err) {
